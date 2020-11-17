17f5f74a2f296badaf6da0929f3d751325d5add0
ziieng <cjengelhardt@gmail.com>
2020-11-17 12:32:24 -0800

    Initial DB schema

---
+++ Emp_DB_schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS empTrack_DB;
+CREATE DATABASE empTrack_DB;
+
+USE empTrack_DB;
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id)
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
