dfaa5dce517eec01d8d62d448de97c78137df9ee
ziieng <cjengelhardt@gmail.com>
2020-11-28 17:25:47 -0800

    Adjust schema for easier queries

--- Emp_DB_schema.sql
+++ Emp_DB_schema.sql
@@ -4,27 +4,27 @@ CREATE DATABASE empTrack_DB;
 USE empTrack_DB;
 
 CREATE TABLE department(
-  id INT NOT NULL AUTO_INCREMENT,
+  d_id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (d_id)
 );
 
 CREATE TABLE role(
-  id INT NOT NULL AUTO_INCREMENT,
+  r_id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NOT NULL,
   department_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+  PRIMARY KEY (r_id),
+  FOREIGN KEY (department_id) REFERENCES department(d_id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT,
+  e_id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  PRIMARY KEY (e_id),
+  FOREIGN KEY (role_id) REFERENCES role(r_id) ON DELETE CASCADE,
+  FOREIGN KEY (manager_id) REFERENCES employee(e_id)
 );
