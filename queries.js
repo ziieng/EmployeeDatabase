99b1d4b7c80ad2307ebd553944c5f435a8fbea33
ziieng <cjengelhardt@gmail.com>
2020-12-01 12:14:49 -0800

    move role and department queries to class file

--- queries.js
+++ queries.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 class Options {
   //never got the queries to work from here
-  constructor(connection)   {
+  constructor(connection) {
     this.connection = connection;
   }
 
@@ -13,11 +13,41 @@ class Options {
   FROM 
     department 
   ORDER BY 
-    name ASC`
-    return query
+    name ASC`;
+    return query;
   }
 
-  employeeList(constraint,sort)   {
+  roleList(constraint, sort) {
+    //list of roles
+    if (constraint != "") {
+      constraint = `
+      WHERE 
+        ${constraint}`;
+    }
+    let query = `SELECT 
+        CONCAT(last_name, ", ", first_name) AS 'Name',
+        title AS 'Role',
+        name AS 'Department',
+        salary AS 'Salary',
+        e_id
+      FROM 
+        employee 
+      LEFT JOIN
+        role
+      ON
+        employee.role_id=role.r_id
+      LEFT JOIN 
+        department 
+      ON 
+        role.department_id = department.d_id
+      ${constraint}
+      ORDER BY
+        ${sort}`;
+
+    return query;
+  }
+
+  employeeList(constraint, sort) {
     //list of employees
     if (constraint != "") {
       constraint = `
@@ -43,10 +73,9 @@ class Options {
       ${constraint}
       ORDER BY
         ${sort}`;
-    
+
     return query;
   }
-
 }
 
   module.exports = Options;
